14534199910aaade8af0d68d61ff41699c21d325
Thinger <trapspring16@gmail.com>
2025-01-07 06:30:43 +0000

    Improved CameraController a bit

--- src/KnitControllers/CameraController.lua
+++ src/KnitControllers/CameraController.lua
@@ -1,4 +1,4 @@
---!nonstrict
+--!nocheck
 
 local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
@@ -7,6 +7,7 @@ local Knit = require(ReplicatedStorage.Packages.Knit)
 local Component = require(ReplicatedStorage.Packages.Component)
 
 local LocalPlayer = Players.LocalPlayer
+local CurrentCamera = workspace.CurrentCamera
 local CameraPivotOffset = Vector3.new(0, 0.25, 0.15)
 
 local CameraController = Knit.CreateController({
@@ -41,14 +42,14 @@ local function GetClosestPlayer(cameraObject: MeshPart): Player?
 	return closestTarget
 end
 
-local function LookAtTarget(cameraContainer: Model, target: Player)
+local function LookAtTarget(cameraContainer: Model, target: Player, delta: number)
 	local cameraObject: MeshPart = cameraContainer.Camera
 	local targetPart: Part = target.Character.Head
 	local targetPosition: Vector3 = targetPart.Position
 
 	local startingCFrame = CFrame.new(cameraObject.Position + CameraPivotOffset)
-	local endingCFrame = startingCFrame:Lerp(CFrame.new(startingCFrame.Position, targetPosition), 1)
-	local x, y, _ = endingCFrame:ToOrientation()
+	local endingCFrame = cameraObject.CFrame:Lerp(CFrame.new(startingCFrame.Position, targetPosition), delta)
+	local x, y = endingCFrame:ToOrientation()
 
 	cameraObject.Orientation = Vector3.new(math.max(math.deg(x), -45), math.deg(y), 0)
 end
@@ -56,6 +57,10 @@ end
 function CameraController:KnitInit()
 	local CameraComponent = Component.new({ Tag = "Camera", Ancestors = { workspace } })
 	CameraComponent.Started:Connect(function(component)
+		local cameraObject: MeshPart? = component.Instance:FindFirstChild("Camera")
+		if cameraObject then
+			component.OriginalCFrame = cameraObject.CFrame
+		end
 		table.insert(CameraController.Cameras, component)
 	end)
 	CameraComponent.Stopped:Connect(function(component)
@@ -67,18 +72,22 @@ function CameraController:KnitInit()
 end
 
 function CameraController:KnitStart()
-	RunService:BindToRenderStep("Cameras", Enum.RenderPriority.Last.Value, function()
+	RunService:BindToRenderStep("Cameras", Enum.RenderPriority.Last.Value, function(delta: number)
+		delta = math.min(1, delta * 15)
 		for _, camera in CameraController.Cameras do
 			local container = camera.Instance
 			local cameraObject: MeshPart? = container:FindFirstChild("Camera")
-			if not cameraObject or LocalPlayer:DistanceFromCharacter(cameraObject.Position) > 75 then
+			if not cameraObject or (CurrentCamera.CFrame.Position - cameraObject.Position).Magnitude > 75 then
 				continue
+			elseif cameraObject and not camera.OriginalCFrame then
+				camera.OriginalCFrame = cameraObject.CFrame
 			end
 			local target = GetClosestPlayer(cameraObject)
 			if not target then
+				cameraObject.CFrame = cameraObject.CFrame:Lerp(camera.OriginalCFrame, delta)
 				continue
 			end
-			LookAtTarget(container, target)
+			LookAtTarget(container, target, delta)
 		end
 	end)
 end
